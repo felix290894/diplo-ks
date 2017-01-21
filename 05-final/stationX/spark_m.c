9aed43acbd8d523ca57b8b5c1450a18bdb4cdcf0
Felix Ruiz <chrisu2dell@yahoo.com>
2017-01-21 05:06:38 -0600

    cleaning leftovers

--- 05-final/stationX/spark_m.c
+++ 05-final/stationX/spark_m.c
@@ -300,28 +300,3 @@ static struct spark_algorithm spfun_algorithm = {
 };
 
     module_i2c_driver(spark_driver);
-    /*
-static u32 spfun_func(struct spark_adapter *adapter)
-{
-  return I2C_FUNC_SMBUS_QUICK |
-         I2C_FUNC_SMBUS_BYTE |
-         I2C_FUNC_SMBUS_BYTE_DATA |
-         I2C_FUNC_SMBUS_WORD_DATA |
-         I2C_FUNC_SMBUS_BLOCK_DATA;
-}
-
-static s32 spfun_access(struct spark_adapter *adap,
-			u16 addr,
-			unsigned short flags,
-			char read_write,
-			u8 command,
-			int size,
-			union i2c_smbus_data *data)
-{
-  int i, len;
-  dev_info(&adap->dev, "%s was called with the"
-	   "following parameters:\n",
-	   __FUNCTION__);
-  dev_info(&adap->dev, "addr = %.4x\n",addr);
-  dev_info(&adap->swv, "flags = %.4x\n",flags)
-  }*/
